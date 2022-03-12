c67bbda7107e2116c541c4d89372095a0d938c13
yazidox <yazidkherrati@gmail.com>
2022-03-12 15:51:18 +0100

    Updates

---
+++ _next/static/yRDPJy8XL19PaObw3cw57/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-84f0f6270bb5e660ca11.js"],"/_error":["static/chunks/pages/_error-077790e38fbfe3891eb2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
